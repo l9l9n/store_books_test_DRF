315e628351f0c50df9aad6d2788f7a88f793cc84
l9l9n_PC <l9l9ndos@gmail.com>
2026-01-12 04:55:54 +0600

    Step 2

---
+++ books/store/tests/test_api.py
@@ -0,0 +1,18 @@
+from django.db.migrations import serializer
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from store.models import Book
+from store.serializers import BookSerializer
+
+
+class BooksApiTestCase(APITestCase):
+    def test_get(self):
+        book_1 = Book.objects.create(name='Test book 1', price=10)
+        book_2 = Book.objects.create(name='Test book 2', price=20)
+        url = reverse('book-list')
+        response = self.client.get(url)
+        serializer_data = BookSerializer([book_1, book_2], many=True).data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer_data, response.data)
